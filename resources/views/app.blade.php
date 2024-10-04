3ddc8b8972c16e1bb0dc79f29b97034e083ea20c
BintangDiLangit <syekhuda463@gmail.com>
2024-10-04 15:49:51 +0700

    update meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,40 @@
 
     <title inertia>{{ config('app.name', 'BintangDiLangit') }}</title>
 
+    <!-- Basic Meta -->
+    <meta name="title" content="Docs - Bintang Miftaqul Huda">
+    <meta name="description"
+        content="Documentation and Resources of Bintang Miftaqul Huda - BINTANGMFHD">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Bintang Miftaqul Huda - Software Engineer">
+    <meta property="og:description"
+        content="Documentation and Resources of Bintang Miftaqul Huda - BINTANGMFHD">
+    <meta property="og:image" content="https://is3.cloudhost.id/portfolio123/storage/seo/64e0bb014746fpueucwmxjg.webp">
+    <meta property="og:url" content="https://bintangmfhd.com/">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://bintangmfhd.com/">
+    <meta property="twitter:title" content="Bintang Miftaqul Huda - Software Engineer">
+    <meta property="twitter:description"
+        content="Documentation and Resources of Bintang Miftaqul Huda - BINTANGMFHD">
+    <meta property="twitter:image"
+        content="https://is3.cloudhost.id/portfolio123/storage/main_image/main_image64de2f2450993xw9d1.jpg">
+
+    <!-- SEO Meta -->
+    <meta name="keywords"
+        content="bintangmfhd, resume, cv, vCard, portfolio, software engineer, programmer, Bintang Miftaqul Huda, documentation, docs">
+    <meta name="robots" content="index, follow, noodp">
+    <meta name="googlebot" content="index, follow">
+    <meta name="google" content="notranslate">
+    <link rel="canonical" href="https://bintangmfhd.com/">
+
+    <!--Favicon-->
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon-16x16.png') }}">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
